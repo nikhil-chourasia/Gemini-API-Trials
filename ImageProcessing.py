d6ff41582be5129f594fd0aef05cee44916cf64d
Nikhil Chourasia <nikhil24chourasia@gmail.com>
2025-03-06 00:00:05 +0530

    Image Processing Done

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -10,7 +10,9 @@ response = client.models.generate_content(
     model="gemini-2.0-flash",
     contents=["What is this image?", image]
 )
+
 print("Printing Response...")
 with open("response.md", "w") as file:
     file.write(response.text)
+
 print("The response for this image is stored in response.md")
