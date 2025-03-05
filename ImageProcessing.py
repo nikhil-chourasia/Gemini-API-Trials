fd1fbdf2959125dfc050247271a5150f1e783d36
Nikhil Chourasia <nikhil24chourasia@gmail.com>
2025-03-05 23:58:54 +0530

    Image Processing Done

---
+++ ImageProcessing.py
@@ -0,0 +1,16 @@
+from google import genai 
+from google.genai import types
+from PIL import Image
+
+image = Image.open('./path/to/file')
+
+print("Reading Image....")
+client = genai.Client(api_key='GEMINI-API-KEY')
+response = client.models.generate_content(
+    model="gemini-2.0-flash",
+    contents=["What is this image?", image]
+)
+print("Printing Response...")
+with open("response.md", "w") as file:
+    file.write(response.text)
+print("The response for this image is stored in response.md")
